bda1b3de6a21ad335124aad6183f36b3639388c0
Asianguy123 <sas@drsyed.org>
2021-11-08 00:32:32 +0000

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,16 @@
+# Shifts and Mask operations
+
+# --------------------------------------------------------------------------------------------------
+# Logical Shifts
+# 2**x, x>0: shifts <==  multiplying
+# 2**x, x<0: shifts ==> dividing
+
+# Arithmetic Shifts
+# multiplying and dividing but for Two's Complement 
+# retains leading 1s when shifting a negative binary number
+
+# Masking
+# AND - used to turn off bits: input(10011010) + mask(11100110) = output(10000010)
+# OR - used to turn on bits: input(10011010) + mask(11100110) = output(1111110)
+
+# --------------------------------------------------------------------------------------------------
