d6b9f159948c7d12a7801103c3e3d17de6e1addd
Asianguy123 <sas@drsyed.org>
2021-11-22 09:32:25 +0000

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,3 +4,16 @@
 import sys
 import pygame
 from operations import *
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Utility Functions
+
+def draw_text(text, font, color, surface, x, y):
+    '''
+    Outputs text at the specified location, with (x,y) being the centre of the text object, using the required parameters
+    '''
+
+    text_obj = font.render(text, 1, color)
+    text_rect = text_obj.get_rect()
+    text_rect.center = (x, y)
+    surface.blit(text_obj, text_rect)
